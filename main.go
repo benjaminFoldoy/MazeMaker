9d24449dba4da9206774d83187b130567a9e7ff4
benjaminFoldoy <61045636+benjaminFoldoy@users.noreply.github.com>
2022-08-05 07:52:12 +0200

    Copy pasted project from local to github

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"$HOME/tile"
+	"fmt"
+)
+
+//############################################################
+//Name: Main
+//Date: Who knows
+//Time:
+//############################################################
+//Dependencies: Everything
+//Description : Everything
+//############################################################
+func main() {
+	//Create the maze struct
+
+	fmt.Println(tile.SayHello())
+
+	/*maze := Maze{
+		size:             Size{width: 10, height: 10},
+		minNumberOfTurns: 4,
+	}
+	maze.create("Test")
+	*/
+}
