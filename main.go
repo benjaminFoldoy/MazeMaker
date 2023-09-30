bbdd0a37c8083d74adf71d176b80ae749dfeb990
Benjamin Foldøy Grimstad <61045636+benjaminFoldoy@users.noreply.github.com>
2023-09-30 20:44:23 +0200

    Fine tuned weight of values

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 //This is done by importing the package, and then using the package name as a prefix.
 import (
 	"MazeModule/maze"
+	"math"
 	"os"
 	"strconv"
 	"time"
@@ -17,16 +18,8 @@ import (
 // Dependencies: Everything
 // Description : Everything
 // ############################################################
-// letting us pass arguments when calling main from console, with default values:
-// to pass values for width, height, minNumberOfTurns, maxLength and margin, we need to pass them as arguments when calling main from console.
-// Example: go run main.go 20 60 70 3 30
 func main() {
 	//Create the maze struct
-	width, height, minNumberOfTurns, maxLength, margin := 20, 60, 2, 3, 30
-	minNumberOfTurns = width*height/100 + 1
-
-	//convert the arguments from string to int
-	//if the conversion fails, the default values will be used.
 	if len(os.Args) < 4 {
 		panic("Not enough arguments, using default values\n")
 	}
@@ -34,9 +27,13 @@ func main() {
 	h, error2 := strconv.Atoi(os.Args[2])
 	n := os.Args[3]
 
+	//convert the arguments from string to int
+	//if the conversion fails, the default values will be used.
+
 	if w < 10 || h < 10 {
 		panic("Width and height must be at least 10")
 	}
+	width, height, margin := 20, 60, 30
 
 	if error1 == nil {
 		width = w
@@ -44,12 +41,19 @@ func main() {
 	if error2 == nil {
 		height = h
 	}
+	minNumberOfTurns := int(math.Sqrt(float64(width*height)) * 2)
+	maxLength := int(min(min(width, height)/3+1, 12))
+
 	maze := maze.Maze{
 		Size:             maze.Size{Width: width, Height: height},
 		MinNumberOfTurns: minNumberOfTurns,
 		MaxLength:        maxLength,
 		Margin:           margin,
 	}
-	maze.Create("output_images/"+n, time.Now().UnixNano())
+	//maze.Create("output_images/"+n, )
+	running := true
+	for running {
+		running = !maze.Create("output_images/"+n, time.Now().UnixNano())
+	}
 	print("\033[32mSuccess!\033[35m\nCreated image at path: output_images/" + n + "\n\033[0m")
 }
